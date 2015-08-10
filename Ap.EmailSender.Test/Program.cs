88025a5da12760da6456a282c351421aecbaf103
apozhidaev <alexander.pozhidaev@gmail.com>
2015-08-10 21:40:18 +0300

    hotfix

---
+++ Ap.EmailSender.Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ap.EmailSender.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var viewEngine = new ViewEngine();
+            var bodyProvider = new TemplateProvider(new[] { new EmailTemplate(typeof(TestMail), "<b>{{Value}}</b>") });
+            var subjectProvider = new TemplateProvider(new[] { new EmailTemplate(typeof(TestMail), "{{Title}}") });
+            var emailSender = new EmailSender(new EmailRenderer(bodyProvider, viewEngine), new EmailRenderer(subjectProvider, viewEngine), new TestSmtpService());
+            emailSender.Send(new[] { "tt" }, new TestMail { Value = "777", Title = "555"});
+            Console.ReadKey();
+        }
+    }
+
+    public class TestMail
+    {
+        public string Title { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class TestSmtpService : ISmtpService
+    {
+        public void Send(string[] recipients, string subject, string body, string[] attachments = null)
+        {
+            foreach (var recipient in recipients)
+            {
+                Console.WriteLine(recipient);
+            }
+            Console.WriteLine(subject);
+            Console.WriteLine(body);
+        }
+    }
+}
